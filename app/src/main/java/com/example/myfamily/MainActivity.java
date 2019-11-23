161669148c77b4f6f8f6cddef99fa963ce7e464e
Bottle1985 <mrboongvn@yahoo.com.vn>
2019-11-23 17:41:01 +0700

    - Test button function

--- app/src/main/java/com/example/myfamily/MainActivity.java
+++ app/src/main/java/com/example/myfamily/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText Name;
     DBHelper mybd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Name= (EditText) findViewById(R.id.editText);
     }
     public void onClickTest(View view)
     {
+        Name.setText("Bông is handsome");
         Toast.makeText(getApplicationContext(), "Test Successfully",
                 Toast.LENGTH_SHORT).show();
     }
