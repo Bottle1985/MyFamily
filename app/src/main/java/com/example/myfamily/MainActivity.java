264ddf4b9f2704e13df560a40203d40116cf475b
Bottle1985 <mrboongvn@yahoo.com.vn>
2019-11-24 16:46:51 +0700

    + Inserted temp data

--- app/src/main/java/com/example/myfamily/MainActivity.java
+++ app/src/main/java/com/example/myfamily/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfamily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -15,13 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Name= (EditText) findViewById(R.id.editText);
+        mybd = new DBHelper(this);
     }
     public void onClickTest(View view)
     {
         Name.setText("Bông is handsome");
+        mybd.insertContact("Boong","0914451299","mrboongvn@yahoo.com.vn","Doan Tran Nghiep", "28");
         Toast.makeText(getApplicationContext(), "Test Successfully",
                 Toast.LENGTH_SHORT).show();
     }
+    public void onClickLoad(View view)
+    {
+        //means this is the view part not the add contact part.
+        Cursor rs = mybd.getData(1);
 
+        rs.moveToFirst();
+
+        String nam = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_NAME));
+        String phon = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_PHONE));
+        String emai = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_EMAIL));
+        String stree = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STREET));
+        String plac = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_CITY));
+
+        if (!rs.isClosed())  {
+            rs.close();
+        }
+        //Toast.makeText(getApplicationContext(), "Load Successfully",
+         //       Toast.LENGTH_SHORT).show();
+         Toast.makeText(getApplicationContext(),  nam,
+                Toast.LENGTH_SHORT).show();
+
+
+    }
 
 }
