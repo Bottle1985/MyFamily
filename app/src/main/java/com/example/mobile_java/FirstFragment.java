fc47a9f914c216ca18440734d2a1c0f1e5ae4a22
Bottle1985 <mrboongvn@yahoo.com.vn>
2024-10-05 11:36:22 +0700

    + Added list view

--- app/src/main/java/com/example/mobile_java/FirstFragment.java
+++ app/src/main/java/com/example/mobile_java/FirstFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.mobile_java.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
 
--- app/src/main/java/com/example/mobile_java/MainActivity.java
+++ app/src/main/java/com/example/mobile_java/MainActivity.java
@@ -17,6 +17,9 @@ import com.example.mobile_java.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +46,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        final ListView list = findViewById(R.id.list);
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
+        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
     }
 
     @Override
